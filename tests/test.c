9d76a9909d95ec02a7bccd8cd9bf08b8abea40a9
aatbip <anantabipal2010@gmail.com>
2026-05-06 17:10:19 +0545

    Automatic variable incrementing test

--- tests/test.c
+++ tests/test.c
@@ -1,17 +1,21 @@
 #include "../libdill.h"
 #include <stdio.h>
 #include <stdlib.h>
-
-coroutine void worker(const char *text) {
+coroutine void worker(const char t) {
+    int i = 0, j = 0;
     while (1) {
-        printf("%s\n", text);
+        if (t == 'a') {
+            printf("a: %d\n", i++);
+        } else {
+            printf("b: %d\n", j++);
+        }
         msleep(now() + random() % 500);
     }
 }
 
 int main() {
-    go(worker("Hello!"));
-    go(worker("World!"));
+    go(worker('a'));
+    go(worker('b'));
     msleep(now() + 5000);
     return 0;
 }
